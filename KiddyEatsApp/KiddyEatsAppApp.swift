a45a792819e28d285dd1b38c4cbd1297fdbc9c59
Hans Arthur Cupiterson <hansarthur20@gmail.com>
2024-08-15 14:39:55 +0700

    Initial Commit

---
+++ KiddyEatsApp/KiddyEatsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KiddyEatsAppApp.swift
+//  KiddyEatsApp
+//
+//  Created by Hans Arthur Cupiterson on 15/08/24.
+//
+
+import SwiftUI
+
+@main
+struct KiddyEatsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
